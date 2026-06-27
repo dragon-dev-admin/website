bfd8b32bfc51b37a90379a8ab5938afac2cf4419
dragon-dev-admin <baddragonmygoodness@gmail.com>
2026-06-27 12:21:59 -0700

    Update launch video to new Vimeo link

--- src/lib/site-links.ts
+++ src/lib/site-links.ts
@@ -4,7 +4,7 @@ export const SITE_LINKS = {
   chromeExtension:
     "https://chromewebstore.google.com/detail/dragon/ncbgllgbplhnbekllhogabdefjidbkoe",
   githubOrg: "https://github.com/alpha-dragon-org",
-  launchVideo: "https://vimeo.com/1091491580/5491d65057",
+  launchVideo: "https://vimeo.com/1062123553?fl=ip&fe=ec",
   telegram: "https://t.me/alphadragontg",
   email: "mailto:baddragonmygoodness@gmail.com",
   twitter: "https://twitter.com/AlphaDragonAI",
